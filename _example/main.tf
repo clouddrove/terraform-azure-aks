e173fbaaa9b4d707798e46db661f991903e7de04
nileshgadgi <nilesh.gadgi@clouddrove.com>
2023-05-08 20:23:24 +0530

    fix: changed aks module source to local

--- _example/main.tf
+++ _example/main.tf
@@ -62,7 +62,7 @@ module "log-analytics" {
 }
 
 module "aks" {
-  source      = "clouddrove/aks/azure"
+  source      = "../"
   name        = "app"
   environment = "test"
   label_order = ["name", "environment"]
