57ea4382c0f9a195806f775e478ba77f1d3534e0
Ravi Malvia <ravi.malvia@clouddrove.com>
2024-07-27 00:03:48 +0530

    fix: Updated subnet version to 1.2.0

--- _examples/public_cluster/example.tf
+++ _examples/public_cluster/example.tf
@@ -27,7 +27,7 @@ module "vnet" {
 
 module "subnet" {
   source  = "clouddrove/subnet/azure"
-  version = "1.1.0"
+  version = "1.2.0"
 
   name                 = "app"
   environment          = "test"
